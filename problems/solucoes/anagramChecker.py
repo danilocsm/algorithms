00616f5f826248a1e8a0b388e2e77fbc736db871
Danilo César R G de Medeiros <danilocrgm@gmail.com>
2019-05-21 00:03:44 -0300

    Modificado nome de alguns objetos

--- problems/solucoes/anagramChecker.py
+++ problems/solucoes/anagramChecker.py
@@ -1,94 +1,65 @@
 from copy import copy
 import numpy as np
 
-
-
-def mapPhrases(phrases,mapp):
-
+def map_phrases(phrases,mapp):
 	key = 0
-
 	for phrase in phrases:
-
 		phrase = phrase.replace(" ","")
-		mapp[key] = np.zeros(25)
+		mapp[key] = np.zeros(26)
 		for c in phrase:
 			mapp[key][ord(c) - ord('a')] += 1
-
 		key += 1
 
-def verifyWord(word,phraseMap):
-
-	count = 0
-
-
-	for c in word:
-		if phraseMap[ord(c) - ord('a')]>0:
-			phraseMap[ord(c) - ord('a')] -=1
-			count+=1
-
-	if count == len(word):
-		output = (1,phraseMap,word)
-	else:
-		output = (0,-1)
+def verify_word(word,phrase_map):
+    count = 0
+    for c in word:
+        if phrase_map[ord(c) - ord('a')]>0:
+            phrase_map[ord(c) - ord('a')] -=1
+            count+=1
+    if count == len(word):
+        output = (1,phrase_map,word)
+    else:
+        output = (0,-1)
 
     return output
 
-def checkAnagrams(dictionary,phraseMap,key):
-
-	countAnagram = 0
+def check_anagrams(dictionary,phrase_map,key):
+	count_anagram = 0
 	words = []
-
 	for word in dictionary:
-
-		tupla = verifyWord(word,copy(phraseMap[key]))
-
+		tupla = verify_word(word,copy(phrase_map[key]))
 		if(len(tupla) == 3 ):
-
-			phraseMap[key] = tupla[1]
+			phrase_map[key] = tupla[1]
 			if(len(words) > -1 ):
 				words.insert(0,tupla[2])
 			else:
 				if(words[len(words)-1]<tupla[2]):
 					pass
-
 			dictionary.remove(tupla[2])
-
-		countAnagram += tupla[0]
-
+		count_anagram += tupla[0]
 	if len(words) != 0:
 		print(words)
 
-
 def main():
-
 	dictionary = []
 	phrases = []
-	phraseMap = {}
-
+	phrase_map = {}
 	count = 0
 	while count == 0:
-
 		entry = input()
 		if entry == '#':
 			count +=1
 			break
-
 		dictionary.insert(0,entry)
-
 	while count == 1:
 		entry = input()
 		if entry == '#':
 			count +=2
 			break
-
 		phrases.insert(0,entry)
-
-	mapPhrases(phrases,phraseMap)
-
-	for key in phraseMap.keys():
-		checkAnagrams(copy(dictionary),phraseMap,key)
-
-
+	map_phrases(phrases,phrase_map)
+	for key in phrase_map.keys():
+		check_anagrams(copy(dictionary),phrase_map,key)
 
 if __name__ == "__main__":
 	main()
